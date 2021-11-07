84310ecaa8a21da2a95a8582550cc57979c4f981
ADMIN\Admin <vukhai27091998@gmail.com>
2021-11-07 23:49:58 +0700

    Inheritance

---
+++ ObjectOrientedProgramming/Examp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Examp1
+{
+    class Animal
+    {
+        protected double Weight;
+        protected double Height;
+        protected static int Legs;
+        public void Info()
+        {
+            Console.WriteLine("Weight: " + Weight + " Height: " + Height + " Legs: " + Legs);
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat ()
+        {
+            Weight = 500;
+            Height = 20;
+            Legs = 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat BlackCat = new Cat();
+            BlackCat.Info();
+            Console.ReadKey();
+        }
+    }
+}
