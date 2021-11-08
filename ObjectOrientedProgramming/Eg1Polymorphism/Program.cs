e16353e2bccbe0314707948d097aace31ea45dfa
ADMIN\Admin <vukhai27091998@gmail.com>
2021-11-08 19:56:07 +0700

    Polymorphism Interface

---
+++ ObjectOrientedProgramming/Eg1Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Eg1Polymorphism
+{
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine(" Animal is speaking... ");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine(" Cat is speaking... ");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine(" Dog is speaking... ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat();
+            Animal dog = new Dog();
+            cat.Speak();
+            dog.Speak();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Interface
+{
+    interface ISpeak
+    {
+        // Khai báo phương thức nhưng không định nghĩa nội dung
+        void Speak();
+    }
+    class Animal : ISpeak // lớp Animal thực thi interface ISpeak
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Animal is speaking... ");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISpeak animal = new Animal();
+            animal.Speak();
+            Console.ReadKey();
+        }
+    }
+}
